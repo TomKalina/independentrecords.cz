99ff0ddad29dfd609c61ade91dcd360184a2f08b
marketazadrazilova1@gmail.com <marketazadrazilova1@gmail.com>
2023-10-21 15:04:16 +0200

    odebrala jsem iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,16 @@
 
 export default function Home() {
   return (
-    <iframe
-      src="https://www.youtube.com/embed/Vb0bFl84Bi8?version=3&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;showinfo=0&amp;loop=1;playlist=Vb0bFl84Bi8&amp;quality=1080p;rel=0"
-      allow="fullscreen;"
-      allowFullScreen
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        width: "100%",
-        height: "100%",
-        border: "none",
-        margin: "0",
-        padding: "0",
-        overflow: "hidden",
-        zIndex: 999999,
-      }}
-    ></iframe>
+    <html>
+      <head>
+        
+
+
+      </head>
+      <body>
+        ahoj
+        ahoj
+      </body>
+    </html>
   );
 }
