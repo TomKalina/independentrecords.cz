aecf47e7c49a786a5fc068822f65a490e77e6e65
Tonda (Tomáš) Kalina <kalina@shoptet.cz>
2024-05-28 09:01:45 +0200

    refactor: Update Services component images

--- src/app/subPages/Services.tsx
+++ src/app/subPages/Services.tsx
@@ -20,9 +20,9 @@ export default function Services(props: Props) {
       </p>
       <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-7xl">
         <div className="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-4">
-          <Box title="Nahrávací den" text="" src="/ai/1.jpeg" />
+          <Box title="Nahrávací den" text="" src="/ai/3.jpeg" />
           <Box title="Mixing" text="" src="/ai/2.jpeg" />
-          <Box title="Mastering" text="" src="/ai/3.jpeg" />
+          <Box title="Mastering" text="" src="/ai/1.jpeg" />
           <Box title="Produkční služby" text="" src="/ai/4.jpeg" />
         </div>
         <p className="text-center">
