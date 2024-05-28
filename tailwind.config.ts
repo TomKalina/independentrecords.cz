3b9ccc18411e6c4c7325512aa35a8c3acb0f40b5
Tonda (Tomáš) Kalina <kalina@shoptet.cz>
2024-05-28 15:05:59 +0200

    refactor: Update Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,33 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#010717",
+      primary: "#004cfd",
+      secondary: "#5123f5",
+      gray: {
+        lighter: "#FAF7F3",
+        light: "#323232",
+        dark: "#010717",
+        txt: "#4c4d56",
+      },
+    },
   },
   plugins: [require("tailwindcss-animated")],
 };
