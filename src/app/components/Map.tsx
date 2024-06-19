3adce6d51be8a137bd73fcd822b0ad1c67d8a31c
Tonda (Tomáš) Kalina <kalina@shoptet.cz>
2024-06-19 08:08:54 +0200

    refactor: Update Map component with more precise coordinates

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -2,7 +2,7 @@
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 function App() {
-  const position = { lat: 49.9055416, lng: 16.4493639 };
+  const position = { lat: 49.90791896078289, lng: 16.447756930036526 };
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
   return (
     apiKey && (
