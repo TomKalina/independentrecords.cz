597d86b4595fe94adffbc5807d91837d8c0c8f42
Tonda (Tomáš) Kalina <kalina@shoptet.cz>
2024-07-20 20:02:34 +0200

    refactor: Update Menu component to improve accessibility and responsiveness

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,25 +1,94 @@
+"use client";
+
 import * as React from "react";
 
 interface Props {}
 
+const menu = [
+  { href: "#pribeh", label: "Náš příběh" },
+  { href: "#sluzby", label: "Služby a vybavení" },
+  { href: "#vybaveni", label: "Vybavení studia" },
+  { href: "#kontakty", label: "Kontakt" },
+];
+
 function Item(props: { href: string; children: React.ReactNode }) {
   return (
-    <li className="hover:animate-wiggle hover:animate-duration-200 hover:animate-thrice">
-      <a href={props.href} className="font-bold hover:text-secondary">
+    <li className=" hover:animate-wiggle hover:animate-duration-200 hover:animate-thrice ">
+      <a href={props.href} className="font-bold text-white hover:text-primary">
         {props.children}
       </a>
     </li>
   );
 }
-export default function Menu(props: Props) {
+
+function MobileItem(props: { href: string; children: React.ReactNode }) {
   return (
-    <nav className="hidden justify-center md:flex md:flex-grow">
-      <ul className="flex justify-center space-x-4 text-white ">
-        <Item href="#pribeh">Náš příběh</Item>
-        <Item href="#sluzby">Služby a vybavení</Item>
-        <Item href="#vybaveni">Vybavení studia</Item>
-        <Item href="#kontakty">Kontakt</Item>
-      </ul>
-    </nav>
+    <li className="border-b border-primary pb-4 pt-4 ">
+      <a href={props.href} className="font-bold text-white hover:text-primary">
+        {props.children}
+      </a>
+    </li>
+  );
+}
+
+export default function Menu(props: Props) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="hidden justify-center sm:hidden md:flex md:flex-grow">
+        <ul className="flex justify-center space-x-4 ">
+          {menu.map((item) => (
+            <Item key={item.href} href={item.href}>
+              {item.label}
+            </Item>
+          ))}
+        </ul>
+      </nav>
+      <button
+        className="flex text-white focus:outline-none md:hidden"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        {isMenuOpen ? (
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 6l12 12M6 18L18 6"
+            ></path>
+          </svg>
+        ) : (
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        )}
+      </button>
+      {isMenuOpen && (
+        <nav className="w-full md:block md:w-auto">
+          <ul>
+            {menu.map((item) => (
+              <MobileItem key={item.href} href={item.href}>
+                {item.label}
+              </MobileItem>
+            ))}
+          </ul>
+        </nav>
+      )}
+    </>
   );
 }
