54bafbdd038110b710978ec63ef336b2fdad7289
IndependentRecords <146770799+IndependentRecords@users.noreply.github.com>
2025-04-02 16:34:33 +0200

    bouda_change_blabla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       <Services />
       <SoundCloud />
       <Equipment />
-      {/* <Review /> */}
+      <Review />
       <Section id="kontakty" title="Kontakt">
         <div className="mx-auto mt-8 max-w-md space-y-4">
           <div className="rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900 dark:text-gray-100">
